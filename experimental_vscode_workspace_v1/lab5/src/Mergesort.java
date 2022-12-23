9c500953a368464322034678e0128b7704dd6888
AxelJohnssonn <mr.axjo3@gmail.com>
2022-12-23 14:47:07 +0100

    Update Mergesort.java

--- experimental_vscode_workspace_v1/lab5/src/Mergesort.java
+++ experimental_vscode_workspace_v1/lab5/src/Mergesort.java
@@ -1,58 +1,96 @@
-
-
 import java.util.Arrays;
 
 public class Mergesort {
+	
+	public static void sort(int[] a) {
+		int[] tmpArray = new int[a.length];
+		mergeSort(a,tmpArray, 0, a.length - 1);
+	}
+	
+	private static void mergeSort(int[] a, int[] tmpArray, int first, int last) {
+		if (first < last) {
+			int mid = first + (last - first) / 2;
+			mergeSort(a, tmpArray, first, mid);
+			mergeSort(a, tmpArray, mid + 1, last);
+			merge(a, tmpArray, first, mid + 1, last);
+		}
+	}
+	
+	private static void merge(int[] a, int[] tmpArray, int leftPos, 
+			                  int rightPos, int rightEnd) {
+		int leftEnd = rightPos - 1;
+		int tmpPos = leftPos;
+		int leftStart = leftPos;
+		// SÃ¥ lÃ¤nge element "finns kvar" i bÃ¤gga halvorna
+		//    vÃ¤lj minsta och lÃ¤gg in tmpArray
+		while(leftPos <= leftEnd && rightPos <= rightEnd) {
+			if (a[leftPos] <= a[rightPos]) {
+				tmpArray[tmpPos++] = a[leftPos++];
+			} else {
+				tmpArray[tmpPos++] = a[rightPos++];
+			}
+		}
+		// Kopiera ev resterande element frÃ¥n vÃ¤nster halva till tmpArray
+		while (leftPos <= leftEnd) {
+			tmpArray[tmpPos++] = a[leftPos++];
+		}
+		// Kopiera ev resterande element frÃ¥n hÃ¶ger halva till tmpArray
+		while (rightPos <= rightEnd) {
+			tmpArray[tmpPos++] = a[rightPos++];
+		}
+		// Kopiera  elementen frÃ¥n tmpArray till a
+		for (int i = leftStart; i <= rightEnd; i++) {
+			a[i] = tmpArray[i];
+		}
+	}
+	
+	
+	public static void sort(Object[] a) {
+		Object[] tmpArray = new Object[a.length];
+		mergeSort(a,tmpArray, 0, a.length - 1);
+	}
+	
+	private static void mergeSort(Object[] a, Object[] tmpArray, int first, int last) {
+		if (first < last) {
+			int mid = (first + last) / 2;
+			mergeSort(a, tmpArray, first, mid);
+			mergeSort(a, tmpArray, mid + 1, last);
+			merge(a, tmpArray, first, mid + 1, last);
+		}
+	}
+	
+	private static void merge(Object[] a, Object[] tmpArray, 
+			int leftPos, int rightPos, int rightEnd) {
+		int leftEnd = rightPos - 1;
+		int tmpPos = leftPos;
+		int leftStart = leftPos;
+		while(leftPos <= leftEnd && rightPos <= rightEnd) {
+			if (((Comparable) a[leftPos]).compareTo(a[rightPos]) <= 0) {
+				tmpArray[tmpPos++] = a[leftPos++];
+			} else {
+				tmpArray[tmpPos++] = a[rightPos++];
+			}
+		}
+		while (leftPos <= leftEnd) {
+			tmpArray[tmpPos++] = a[leftPos++];
+		}
+		while (rightPos <= rightEnd) {
+			tmpArray[tmpPos++] = a[rightPos++];
+		}
+		for (int i = leftStart; i <= rightEnd; i++) {
+			a[i] = tmpArray[i];
+		}
+	}
+	
+	
+	public static void main(String[] args) {
+		Integer[] nbrs = {7, 2, 5, 9, 3, 8, 10, 2};
+		Mergesort.sort(nbrs);
+		System.out.println(Arrays.toString(nbrs));
 
-    public static void main(String[] args) {
-        int[] a = {1000, 1, 300, 90, 70, 2};
-        int[] sorted = mergesort(a);
-        
-        for(int i = 0; i < sorted.length; i++){
-            System.out.println(sorted[i]);
-        }
-    }
+		String[] words = {"BBBB", "CCC","AAAAA", "E", "DD"};
+		Mergesort.sort(words);
+		System.out.println(Arrays.toString(words));
+	}
 
-    public static int[] mergesort(int[] a){
-        if(a.length == 1){
-            return a;
-        }
-
-        int half = a.length/2;
-        int[] left = Arrays.copyOfRange(a, 0, half);
-        int[] right = Arrays.copyOfRange(a, half, a.length);
-
-        left = mergesort(left);
-        right = mergesort(right);
-
-        int[] sorted = new int[a.length];
-        int sorted_index = 0; 
-        int left_index = 0; 
-        int right_index = 0; 
-
-        while(left_index < left.length && right_index < right.length){
-            if(left[left_index] <= right[right_index]){
-                sorted[sorted_index] = left[left_index];
-                left_index++;
-            }else{
-                sorted[sorted_index] = right[right_index];
-                right_index++;
-            }
-        }
-
-        while(left_index < left.length){
-            sorted[sorted_index] = left[left_index];
-                left_index++;
-                sorted_index++;
-        }
-
-        while(right_index < right.length){
-            sorted[sorted_index] = right[right_index];
-                right_index++;
-                sorted_index++;
-        }
-
-        return sorted;
-
-    }
 }
